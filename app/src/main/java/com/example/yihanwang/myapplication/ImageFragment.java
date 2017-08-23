ffe9369b8daa44a7b18be62ad7ddcea832245c7e
KaleyXZ <zheng.kaley.x@gmail.com>
2017-08-24 08:13:19 +1000

    new Branch

--- app/src/main/java/com/example/yihanwang/myapplication/ImageFragment.java
+++ app/src/main/java/com/example/yihanwang/myapplication/ImageFragment.java
@@ -3,9 +3,11 @@ package com.example.yihanwang.myapplication;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class ImageFragment extends Fragment {
 
@@ -16,48 +18,57 @@ public class ImageFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.image_fragment, container, false);
-//        ImageButton imageBtn1 = (ImageButton)view.findViewById(R.id.imageBtn1);
-//        imageBtn1.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View view) {
-////                Intent showInf = new Intent(ImageFragment.this, ImageInformationActivity.class);
-////                showInf.putExtra("PLANT1","Plant:Gold Dust Wattle. Description:Straggling shrub to 2 m high and 1.5 m diameter. Phyllodes are variable, usually less than 2 cm long; narrow to orbicular. Flowers like golden balls appear in spring.");
-////                startActivity(showInf);
-//            }
-//        });
-//
-//        ImageButton imageBtn2 = (ImageButton)view.findViewById(R.id.imageBtn2);
-//        imageBtn2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                Intent showInf = new Intent(getApplicationContext() , ImageInformationActivity.class);
-////                showInf.putExtra("PLANT2","Plant:Hickory Wattle. Description: Small to medium-sized tree to 12 m high with light green, sickle-shaped phyllodes to 20 cm. Bears cream ball-shaped flowers in summer.");
-////                startActivity(showInf);
-//            }
-//        });
-//
-//        ImageButton imageBtn3 = (ImageButton)view.findViewById(R.id.imageBtn3);
-//        imageBtn3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                Intent showInf = new Intent(ImageFragment.this, ImageInformationActivity.class);
-////                showInf.putExtra("PLANT3","Plant:Spike Wattle. Description: A prickly, dense shrub 1-3 m high by 2 m across, small trees are also known. Rigid, dark green phyllodes to 2 cm with sharp point. Flowers in cream spikes occur in winter and spring.");
-////                startActivity(showInf);
-//            }
-//        });
-//
-//        ImageButton imageBtn4 = (ImageButton)view.findViewById(R.id.imageBtn4);
-//        imageBtn4.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                Intent showInf = new Intent(ImageFragment.this, ImageInformationActivity.class);
-////                showInf.putExtra("PLANT4","Plant:Pincushion Lily.Description:The Pincushion Lily produces clumps of brown, branched stems up to 15cm tall, with tufts of spiky leaves, each about 1.5cm long. During springtime, the plant produces attractive round heads of star-shaped white flowers on a stem at the ends of the branches.");
-////                startActivity(showInf);
-//            }
-//        });
+
+        ImageButton imageBtn1 = (ImageButton)view.findViewById(R.id.imageBtn1);
+        imageBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragment = new ImageFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, fragment).addToBackStack(null).commit();
+            }
+        });
+
+        ImageButton imageBtn2 = (ImageButton)view.findViewById(R.id.imageBtn2);
+        imageBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragment = new ImageFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, fragment).addToBackStack(null).commit();
+            }
+        });
+
+        ImageButton imageBtn3 = (ImageButton)view.findViewById(R.id.imageBtn3);
+        imageBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragment = new ImageFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, fragment).addToBackStack(null).commit();
+            }
+        });
+
+        ImageButton imageBtn4 = (ImageButton)view.findViewById(R.id.imageBtn4);
+        imageBtn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragment = new ImageFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, fragment).addToBackStack(null).commit();
+            }
+        });
 
         return view;
 
--- app/src/main/java/com/example/yihanwang/myapplication/ImageInformationActivity.java
+++ app/src/main/java/com/example/yihanwang/myapplication/ImageInformationActivity.java
@@ -1,38 +1,37 @@
 package com.example.yihanwang.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v4.app.Fragment;
 
-public class ImageInformationActivity extends AppCompatActivity {
-    private String plantNo;
-
+public class ImageInformationActivity extends Fragment {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_information);
-        if (getIntent().hasExtra("PLANT1")) {
-            TextView tv = (TextView) findViewById(R.id.plantText);
-            String text = getIntent().getExtras().getString("PLANT1");
-            tv.setText(text);
-        }
-        if (getIntent().hasExtra("PLANT2")) {
-            TextView tv = (TextView) findViewById(R.id.plantText);
-            String text = getIntent().getExtras().getString("PLANT2");
-            tv.setText(text);
-        }
-        if (getIntent().hasExtra("PLANT3")) {
-            TextView tv = (TextView) findViewById(R.id.plantText);
-            String text = getIntent().getExtras().getString("PLANT3");
-            tv.setText(text);
-        }
-        if (getIntent().hasExtra("PLANT4")) {
-            TextView tv = (TextView) findViewById(R.id.plantText);
-            String text = getIntent().getExtras().getString("PLANT4");
-            tv.setText(text);
-        }
-
-
-
     }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.image_information_fragment);
+//        if (getIntent().hasExtra("PLANT1")) {
+//            TextView tv = (TextView) findViewById(R.id.plantText);
+//            String text = getIntent().getExtras().getString("PLANT1");
+//            tv.setText(text);
+//        }
+//        if (getIntent().hasExtra("PLANT2")) {
+//            TextView tv = (TextView) findViewById(R.id.plantText);
+//            String text = getIntent().getExtras().getString("PLANT2");
+//            tv.setText(text);
+//        }
+//        if (getIntent().hasExtra("PLANT3")) {
+//            TextView tv = (TextView) findViewById(R.id.plantText);
+//            String text = getIntent().getExtras().getString("PLANT3");
+//            tv.setText(text);
+//        }
+//        if (getIntent().hasExtra("PLANT4")) {
+//            TextView tv = (TextView) findViewById(R.id.plantText);
+//            String text = getIntent().getExtras().getString("PLANT4");
+//            tv.setText(text);
+//        }
+//    }
 }
