d4890a07b03e58ae03cb65fb6522ae982bdf0e4f
KaleyXZ <zheng.kaley.x@gmail.com>
2017-09-19 09:13:03 +1000

    progress bar

--- app/src/main/java/com/example/yihanwang/myapplication/ComparisonActivity.java
+++ app/src/main/java/com/example/yihanwang/myapplication/ComparisonActivity.java
@@ -4,13 +4,17 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.yihanwang.myapplication.entities.ImageGallery;
 import com.example.yihanwang.myapplication.entities.ImageInfo;
 
 public class ComparisonActivity extends Activity {
-    ImageView plantImage;
-    ImageView plantPhoto;
+    private ImageView plantImage;
+    private ImageView plantPhoto;
+
+    private TextView plant;
+    private TextView photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +23,27 @@ public class ComparisonActivity extends Activity {
         plantImage = (ImageView) findViewById(R.id.plantImage);
         plantPhoto = (ImageView) findViewById(R.id.plantPhoto);
 
+        plant = (TextView) findViewById(R.id.plantname);
+        photo = (TextView) findViewById(R.id.yourphoto);
+
+        photo.setText("Your photo");
+
         Bundle b = getIntent().getExtras();
         double index1 = -1;
         int index2 = -1;
-        if (b != null){
+        if (b != null) {
             index1 = b.getDouble("firstImage");
             index2 = b.getInt("secondImage");
 
-            ImageInfo first  = ImageStorage.getInstance().getImagebyId(index1);
+            ImageInfo first = ImageStorage.getInstance().getImagebyId(index1);
+            String com = first.getCommonName();
+
+            if (com.isEmpty()) {
+                plant.setText(first.getName());
+            } else {
+                plant.setText(com + " (" + first.getName() +")");
+            }
+
             plantImage.setImageDrawable(ImageStorage.getInstance().getDrawable(this.getAssets(), first));
             ImageGallery imageGalery = ImageGalleryStorage.getInstance().getImageGallery(first.getId());
             Bitmap bitmap = imageGalery.getImage(index2);
