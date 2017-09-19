bfd15a3d54ec80208a1ac4d3671f49ced2c76de3
KaleyXZ <zheng.kaley.x@gmail.com>
2017-09-19 10:32:27 +1000

    popup message

--- app/src/main/java/com/example/yihanwang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yihanwang/myapplication/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.yihanwang.myapplication;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.yihanwang.myapplication.gps.LocationService;
@@ -16,8 +18,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(true);
+        builder.setTitle("Here is the alert dialog");
+        builder.setMessage("Here is the message");
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+
+        builder.setPositiveButton("Play", new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.show();
+
+
+
+
+
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setHomeButtonEnabled(false);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
