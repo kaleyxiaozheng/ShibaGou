2d35fcfb1ce7e52b90ae182a8e05c78c9cf7773e
KaleyXZ <zheng.kaley.x@gmail.com>
2017-09-19 19:54:14 +1000

    alertdialog done

--- app/src/main/java/com/example/yihanwang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yihanwang/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.yihanwang.myapplication;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -10,6 +12,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.yihanwang.myapplication.gps.LocationService;
 
@@ -21,33 +26,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setCancelable(true);
-        builder.setTitle("Here is the alert dialog");
-        builder.setMessage("Here is the message");
-
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-
-            @Override
-            public void onClick(DialogInterface dialog, int whichButton) {
-                dialog.cancel();
-            }
-        });
-
+        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.maindialog, null);
+        Typeface font1 = Typeface.createFromAsset(getAssets(),"AYearWithoutRain.ttf");
+        Typeface font2 = Typeface.createFromAsset(getAssets(),"BlessingsthroughRaindrops.ttf");
+        TextView title = (TextView) view.findViewById(R.id.title);
+        TextView message = (TextView) view.findViewById(R.id.message);
+        title.setTypeface(font1);
+        message.setTypeface(font2);
+        title.setText("CampingMate");
+        message.setText("There are 20 plants in your area, press play to begin");
+//        builder.setCancelable(true);
+//        builder.setIcon(R.drawable.alertmessageicon);
+//        builder.setMessage("There are 20 plants in your area, press play to begin");
+//
+//
         builder.setPositiveButton("Play", new DialogInterface.OnClickListener(){
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                Fragment fragment = new ImageSelectFragment();
+                Bundle args = new Bundle();
+                args.putDouble("location_lat", LocationService.getInstance().getCurrentLat());
+                args.putDouble("location_lon", LocationService.getInstance().getCurrentLon());
+                fragment.setArguments(args);
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, fragment).addToBackStack(ImageFragment.class.getName()).commit();
             }
         });
-
+        builder.setView(view);
         builder.show();
-
-
-
-
-
-
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setHomeButtonEnabled(false);
@@ -78,6 +86,12 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 1000) {
             LocationService.getInstance(this).locateCurrentLocation();
         }
+    }
+
+    public void showAlert(View view) {
+        AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
+        myAlert.setMessage("Hello").create();
+        myAlert.show();
     }
 
 }
